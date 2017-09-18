6f17c10f7afdf642adf7f4593d49c27c0140c81e
Dale Ansley Lim <daleansleylim@Dales-MacBook-Air.local>
2017-09-18 23:16:28 +0800

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  	selector: 'app-root',
+  	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	template: `<h1>{{title}}</h1><h2>{{hero}}</h2>`
 })
 export class AppComponent {
-  title = 'app';
+	title = 'Tour of Heroes';
+	hero = 'Windstorm';
 }
